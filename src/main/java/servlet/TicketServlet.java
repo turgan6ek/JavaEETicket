cd627bee5d54bdae453fdec1410e4358138e499b
Yerulan <eturganbek01@gmail.com>
2021-03-15 12:23:06 +0600

    1

--- src/main/java/servlet/TicketServlet.java
+++ src/main/java/servlet/TicketServlet.java
@@ -1,11 +1,12 @@
 package servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@WebServlet(value = "ticket")
 public class TicketServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
